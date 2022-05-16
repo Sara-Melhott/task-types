2203d34f83acbf42ef73bf070c7878dc21e23662
Anastasia Petrova <fairy_tale13@bk.ru>
2022-05-16 20:39:46 +0300

    Задание по теме Типы данных

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,15 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+type person = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+
+type employee = person & { company: string };
+
+export function getPersons(): [person];
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +26,4 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+export function personToString(person: person | employee): string;
